12c33d05121775a74cde9b6681b8cbc4f927a77e
Unknown <diocoelho@hotmail.com>
2017-12-13 20:14:35 +0000

    entrada
    semaforo a N

--- unix.h
+++ unix.h
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define UNIXSTR_PATH "/tmp/s.unixstr"
 #define UNIXDG_PATH  "/tmp/s.unixdgx"
@@ -19,28 +20,36 @@ int readline(int fd, char *ptr, int maxlen);
 void ecra(int sockfd);
 void montanha_russa(int sockfd);
 
+
 //variaveis globais
+int conta_cliente; 
+
+//
 struct parque
 {
 
         int max_cliente;
+        int bilhetaria;
         int contador_time;
         int aberto;
+        int perc_prioridade;
 
 } ;
 struct cliente_struct
 {
         int id;
-        int vip;
         int prioridade;
         int tempo_chegou;
         int tempo_saiu;
         int tempo_esperado;
+        int divertimento;
 
 
 };
+// semaforos
+sem_t s_tam_fila_bilheteira;
+sem_t s_tam_max_parque;
 
-
-struct cliente_struct cliente[50];
+struct cliente_struct cliente[50000];
 struct parque simulador;
-pthread_t static t_cliente [50];
+pthread_t static t_cliente [50000];
