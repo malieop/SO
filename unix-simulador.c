b57b52adbdf8d030f0d2701a28e797c64962405a
Susana <susana-catarina96@hotmail.com>
2018-01-02 15:35:41 +0000

    corrigir merge

--- unix-simulador.c
+++ unix-simulador.c
@@ -45,16 +45,6 @@ void main(void)
 
         if (newsockfd < 0) err_dump("server: accept error");
 
-								simulador.perc_prioridade = 10;
-								simulador.max_cliente = 20;
-								simulador.contador_time = 0;
-								simulador.aberto = 1;
-								simulador.perc_cliente_bilhete= 100;
-								simulador.buff_bilheteira[4];
-								simulador.hora_de_abertura= 8;
-								simulador.hora_de_fecho = 9;
-								simulador.divertimento_boolean = 1;
-
         while(strcmp(line, "c") != 0)
         {
                 read(newsockfd, line, MAXLINE);
@@ -70,7 +60,7 @@ void main(void)
         simulador.hora_de_fecho = 9;
         simulador.divertimento_boolean = 1;
 
-								sem_init(&s_cliente_verificado,0,0);
+
 
         //sem_init(&s_tam_fila_bilheteira,0, 10);
         //inicialização dos semaforos
@@ -85,23 +75,26 @@ void main(void)
         sem_init(&s_finish_fury,0,0);
         sem_init(&s_comunicacaofury325,0,0);
         sem_init(&s_sai_takabisha,0,0);
+        sem_init(&s_cliente_verificado,0,0);
 
-								pthread_mutex_init(&mutex_fury,NULL);
-								pthread_mutex_init(&mutex_takabisha,NULL);
-								pthread_mutex_init(&mutex_bilheteira,NULL);
-								pthread_mutex_init(&mutex_comunicacao,NULL);
-								produz_bilheteira = 0;
-								verifica_cliente_takabisha = 0;
-								consome_bilheteira = 0;
+        pthread_mutex_init(&mutex_fury,NULL);
+        pthread_mutex_init(&mutex_takabisha,NULL);
+        pthread_mutex_init(&mutex_bilheteira,NULL);
+        pthread_mutex_init(&mutex_comunicacao,NULL);
 
-								if((pthread_create(&(thread),NULL,(void *)virtualtime, NULL))!=0)
-				        {
-				                err_dump("pthread_create: erro criação thread");
-				        }
-				        else printf("criou timer\n" );
+        //inicialização de variaveis
+        produz_bilheteira = 0;
+        verifica_cliente_takabisha = 0;
+        consome_bilheteira = 0;
+
+        if((pthread_create(&(thread),NULL,(void *)virtualtime, NULL))!=0)
+        {
+                err_dump("pthread_create: erro criação thread");
+        }
+        else printf("criou timer\n" );
 
         tempo_aberto_parque();
-        timersimulador();
+
         if((pthread_create(&(t_cria_booleano_divertimento),NULL,(void *)divertimento,NULL))!=0) {
                 err_dump("pthread_create: erro criação thread");
         }
@@ -150,30 +143,27 @@ void main(void)
                 printf(" │%s ★ À acender as luzes da bilhteira.\n", gettime());
         }
 
-								while(conta_cliente < simulador.max_cliente);
+        while(conta_cliente < simulador.max_cliente);
 
-								for (int i = 0; i < simulador.max_cliente; i++) {
+        for (int i = 0; i < simulador.max_cliente; i++) {
 
                 pthread_join(t_cliente[i], NULL);
                 usleep(150000);
                 //printf("VACAS DE MERDA");
         }
-								usleep(1500000);
+        usleep(1500000);
 
         pthread_join(t_cria_cliente, NULL);
-								simulador.aberto = 0 ;
-								printf("PUTA CHEGUEI AQUI\n");
-								//pthread_join(t_cria_takabisha, NULL);
-								printf("CRL O TAKABISHA NAO ACABOU\n");
-								//pthread_join(t_cria_fury, NULL);
+        simulador.aberto = 0;
+        printf("PUTA CHEGUEI AQUI\n");
+        //pthread_join(t_cria_takabisha, NULL);
+        printf("CRL O TAKABISHA NAO ACABOU\n");
+        //pthread_join(t_cria_fury, NULL);
 
-								/*for(int j = 0; j < num_bilheteiras; j++)
-								{
-																pthread_join(t_bilheteira[j], NULL);
-								}*/
-								printf("FURY325 fechou as portas.\n" );
-								printf("TAKABISHA fechou as portas \n");
-								printf("O PARQUE FECHOU !!\n");
+        
+        printf("FURY325 fechou as portas.\n" );
+        printf("TAKABISHA fechou as portas \n");
+        printf("O PARQUE FECHOU !!\n");
 
         simulador.aberto = 0;
         printf("PUTA CHEGUEI AQUI\n");
